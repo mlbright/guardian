3c9d52ac4e3a767989ed357e1b2c2fc48da85b3b
Martin-Louis Bright <martin.louis.bright@autodesk.com>
2018-07-17 00:18:04 -0400

    removing registered timers

--- lib/Guardian.pm
+++ lib/Guardian.pm
@@ -3,20 +3,22 @@ use Mojo::Base 'Mojolicious';
 
 # This method will run once at server start
 sub startup {
-    my $self = shift;
+  my $self = shift;
 
-    # Load configuration from hash returned by "my_app.conf"
-    my $config = $self->plugin('Config');
+  # Load configuration from hash returned by "my_app.conf"
+  my $config = $self->plugin('Config');
 
-    # Documentation browser under "/perldoc"
-    $self->plugin('PODRenderer') if $config->{perldoc};
+  # Documentation browser under "/perldoc"
+  $self->plugin('PODRenderer') if $config->{perldoc};
 
-    # Router
-    my $r = $self->routes;
+  $self->helper( services => sub { state $services = {} } );
 
-    # Normal route to controller
-    $r->get('/')->to('example#welcome');
-    $r->post('/silence')->to('silence#silence');
+  # Router
+  my $r = $self->routes;
+
+  # Normal route to controller
+  $r->get('/')->to('example#welcome');
+  $r->post('/silence')->to('silence#silence');
 }
 
 1;
--- lib/Guardian/Controller/Silence.pm
+++ lib/Guardian/Controller/Silence.pm
@@ -1,30 +1,53 @@
 package Guardian::Controller::Silence;
 use Mojo::Base 'Mojolicious::Controller';
+use Mojo::IOLoop;
 
 sub silence {
-    my $self = shift;
-    my $payload;
-    eval { $payload = $self->req->json; };
+  my $self    = shift;
+  my $payload = $self->req->json;
 
-    if ($@) {
-        $self->app->log->debug('malformed JSON');
-    }
-    else {
-        if ( exists( $payload->{next_signal} ) ) {
-            my $timeout = $payload->{next_signal};
-            my $id      = Mojo::IOLoop->timer(
-                $timeout => sub {
-                    $self->app->log->debug('did not receive signal...');
-                }
-            );
+  unless ( exists( $payload->{service_name} )
+    && exists( $payload->{notifier} )
+    && exists( $payload->{'next_signal'} ) )
+  {
+    $self->app->log->error('bad JSON');
+    $self->render( text => "bad JSON...\n\n" . $self->req->body );
+    return;
+  }
 
-            $self->render( text => "OK" );
-        }
-        else {
-            $self->app->log->debug('missing next signal');
-            $self->render( text => $self->req->body );
-        }
+  my $timeout      = $payload->{'next_signal'};
+  my $service_name = $payload->{'service_name'};
+  my $notifier     = $payload->{'notifier'};
+
+  my $service;
+  if ( exists( $self->app->services->{$service_name} ) ) {
+    $self->app->log->info("$service_name is registered...");
+    $service = $self->app->services->{$service_name};
+    my $timer_id = $service->{timer_id};
+    Mojo::IOLoop->remove($timer_id);
+    delete $self->app->services->{$service_name};
+    $self->app->log->info("$service_name has averted disaster");
+  }
+  my $timer_id = Mojo::IOLoop->timer(
+    $timeout => sub {
+      my $loop = shift;
+      $self->app->log->info( 'notify X about ' . $service_name );
+
+      $service = $self->app->services->{$service_name};
+      my $timer_id = $service->{timer_id};
+      $loop->remove($timer_id);
+      delete $self->app->services->{$service_name};
     }
+  );
+  $service = {
+    next_signal  => $timeout,
+    service_name => $service_name,
+    notifier     => $notifier,
+    timer_id     => $timer_id,
+  };
+  $self->app->services->{$service_name} = $service;
+
+  $self->render( text => "OK" );
 }
 
 1;
